26365f2ddb46387643e681bf671d6bebf423fa73
julioleao <julio0023@live.com>
2021-04-12 10:19:20 -0300

    UPDATE: mail mailer

---
+++ src/config/mail.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "host": process.env.HOST,
+    "port": process.env.PORT,
+    "user": process.env.USER,
+    "pass": process.env.PASS
+};
+
+
--- src/config/mailer.js
+++ src/config/mailer.js
@@ -2,7 +2,7 @@ const path = require('path');
 const nodemailer = require('nodemailer');
 const hbs = require('nodemailer-express-handlebars');
 
-const { host, port, user, pass } = require('./mail.json');
+const { host, port, user, pass } = require('./mail');
 
 const transport = nodemailer.createTransport({
     host,
